96c159a108f8041494d8afb7667a997e4be4f0df
Ecsky2010 <niconico@111.com>
2019-01-02 17:17:45 +0800

    jan02

---
+++ app/src/main/java/com/example/administrator/yalimasinei/MyAdapter.java
@@ -0,0 +1,10 @@
+package com.example.administrator.yalimasinei;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Administrator on 2019/1/2.
+ */
+
+public class MyAdapter extends ArrayAdapter {
+}
